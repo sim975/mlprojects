325d323675253e95fd4b76ab3188c4d0525fef64
Simran <drsimran69@gmail.com>
2025-02-16 16:00:48 +0530

    Hyperparameter Tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -57,6 +57,6 @@ if __name__=="__main__":
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
     model_trainer=ModelTrainer()
-    print( model_trainer.initiate_model_training(train_arr,test_arr))
+    print( model_trainer.initiate_model_trainer(train_arr,test_arr))
 
     
