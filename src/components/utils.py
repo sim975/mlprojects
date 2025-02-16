95cebc6aea660f4abc5275bcf421327cdea39a51
Simran <drsimran69@gmail.com>
2025-02-16 13:25:57 +0530

    Model Trainer

--- src/components/utils.py
+++ src/components/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import dill
 
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 
 def save_object(file_path, obj):
@@ -12,14 +13,35 @@ def save_object(file_path, obj):
     This function is responsible for saving the object to the specified file path.
     '''
     try:
-       dir_path = os.path.dirname(file_path)
-
-       os.makedirs(dir_path, exist_ok=True)
-       with open(file_path, 'wb') as file_obj:
-          dill.dump(obj, file_obj)
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
 
-        
-                
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    '''
+    Evaluates multiple models and returns their R2 scores.
+    '''
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
